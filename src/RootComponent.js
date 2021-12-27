e67c1aa9e94b867751ff782d887a6e91f201850a
sonam <sonamdhull001@outlook.com>
2021-12-27 15:41:25 +0530

    added Navigation bar

--- src/RootComponent.js
+++ src/RootComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomePage from "./components/NavigationBar/Home/HomePage";
 
 const RootComponent = () => {
     return (
         <div>
-            
+            <HomePage/>
         </div>
     );
 };
